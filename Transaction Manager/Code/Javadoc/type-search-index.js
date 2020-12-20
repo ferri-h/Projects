6ca15dea61adcc045b2884f8528d766c260fe582
ferri-h <53356385+ferri-h@users.noreply.github.com>
2020-12-20 06:46:51 -0500

    Delete type-search-index.js

--- Transaction Manager/Code/Javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"project2","l":"Account"},{"p":"project2","l":"AccountDatabase"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"project2","l":"Checking"},{"p":"project2","l":"Date"},{"p":"project2","l":"MoneyMarket"},{"p":"project2","l":"Profile"},{"p":"project2","l":"RunProject2"},{"p":"project2","l":"Savings"},{"p":"project2","l":"TransactionManager"}]
